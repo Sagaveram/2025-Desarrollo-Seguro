1e11d2b58cf445b0e6cfe14f2e84cf267a76b3a1
Felipe <felipe@DSS.feli>
2025-11-19 22:14:01 -0300

    Agrego pruebas pytest para SQL Injection (debe fallar en main)

--- services/backend/seeds/carga_test.js
+++ services/backend/seeds/carga_test.js
@@ -1,7 +1,53 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
+exports.seed = async function(knex) {
+  // Insertar usuarios (actualiza si ya existen)
+  await knex('users')
+    .insert([
+      {
+        id: 1,
+        username: 'test',
+        email: 'test@example.local',
+        password: 'password',
+        first_name: 'Test',
+        last_name: 'User',
+        activated: true,
+        reset_password_token: null,
+        reset_password_expires: null,
+        invite_token: null,
+        invite_token_expires: null,
+        picture_path: null
+      },
+      {
+        id: 2,
+        username: 'prod',
+        email: 'prod@example.local',
+        password: 'password',
+        first_name: 'Prod',
+        last_name: 'User',
+        activated: true,
+        reset_password_token: null,
+        reset_password_expires: null,
+        invite_token: null,
+        invite_token_expires: null,
+        picture_path: null
+      }
+    ])
+    .onConflict('id')  // Si el ID ya existe
+    .merge();          // actualiza el registro en lugar de fallar
+
+  // Insertar facturas (actualiza si ya existen)
+  await knex('invoices')
+    .insert([
+      { id: 1, userId: 1, amount: 101.00, dueDate: new Date('2025-01-01'), status: 'unpaid' },
+      { id: 2, userId: 1, amount: 102.00, dueDate: new Date('2025-01-01'), status: 'paid' },
+      { id: 3, userId: 1, amount: 103.00, dueDate: new Date('2025-01-01'), status: 'paid' },
+      { id: 4, userId: 2, amount:  99.00, dueDate: new Date('2025-01-01'), status: 'unpaid' }
+    ])
+    .onConflict('id')
+    .merge();
+};
+
+
+/*
 exports.seed = async function(knex) {
 
   await knex('users').insert({
@@ -61,3 +107,4 @@ exports.seed = async function(knex) {
     status: 'unpaid'
    });
 };
+*/
